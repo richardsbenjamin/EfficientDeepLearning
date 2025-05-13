dc6f548397f56798aa38bebb01cb9eed203c9b13
Benjamin RICHARDS <b24richa@sl-tp-br-501.imta.fr>
2025-05-13 08:46:48 +0200

    Grouped model train 501.

--- model_train_group.py
+++ model_train_group.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     test_loader = get_test_cifar10_dataloader()
 
     get_functions = [
-       # "increasing_grouped_densenet121",
-       # "transition_grouped_densenet121",
+        "increasing_grouped_densenet121",
+     	"transition_grouped_densenet121",
         "increasing_transition_grouped_densenet121",
     ]
 
